8fe0538ef9e19f6a18b1cf8b669c2ec1caef0d85
akaraboga <a.karabogaa46@gmail.com>
2024-11-05 10:56:11 +0300

    Add project files.

---
+++ List2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace List2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kahve listeniz kaç elemanlı olsun");//Kulanıcıdan istediği kadar kahve olusturması için eleman değişkeni aldık
+            int eleman=Convert.ToInt32(Console.ReadLine());
+
+            ArrayList Kahve=new ArrayList();//Arraylistimiz
+
+            Console.WriteLine("Kahve isimlerini giriniz.");
+            for (int i = 1; i <=eleman; i++)//Dongümüzü girdiğimiz eleman kadar donderiyoruz
+            {   
+                string kahveler=Console.ReadLine();//Kahveler değişkenini Kahve arrayimizin içine atayark dongu her donduğunde girdiğimiz kahveleri kaydedecek
+                
+                Kahve.Add(kahveler);//Arrayimizin içine girdiğimiz kahve isimlerini kaydediyoruz
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("kahveleriniz : ");
+
+            foreach(string kahveler in Kahve)//Ekrana bastırma
+            {
+                Console.WriteLine(kahveler);
+            }
+            Console.ReadKey();
+        }
+    }
+}
